c48ee9d98f8f70147b594a6851e311feded2de21
diwoe-gti <diwoe5555@gmail.com>
2025-11-24 10:43:39 +0700

    Add files via upload

---
+++ xo.js
@@ -0,0 +1,77 @@
+let playerO = "O" ;
+let playerX = "X" ;
+let currPlayer = playerO ;
+
+//                 0  1  2  3  4  5  6  7  8
+let gameBoard = [ "","","","","","","","",""];
+let gameCells;
+
+let winningConditions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+];
+
+let gameOver = false;
+let restartGameButton;
+
+window.onload = function() {
+    gameCells = document.getElementsByClassName ("game-cell");
+    for (let cell of gameCells) {
+        cell.addEventListener("click", placeCell);
+    }
+    restartGameButton = document.getElementById ("game-restart-button")
+    restartGameButton.addEventListener("click", restartGame);
+}
+
+function placeCell() {
+    if (gameOver) {
+        return;
+    }
+        
+    const index = parseInt(this.getAttribute("data-cell-index"));
+    if (gameBoard[index] != "") {
+        return;
+    }
+
+    this.innerText = currPlayer;
+    gameBoard[index] = currPlayer;
+
+    //เลือกผู้เล่น
+    currPlayer = (currPlayer == playerO) ? playerX : playerO
+    
+    //เช็คคนชนะ
+    checkWinner();
+}
+
+function checkWinner() {
+    for (let winCondition of winningConditions) {
+        let a = gameBoard[winCondition[0]];
+        let b = gameBoard[winCondition[1]];
+        let c = gameBoard[winCondition[2]];
+        //อัปเดตเซลล์ผู้ชนะ
+        if (a == b && b == c && a != "") {
+            for (i = 0; i < gameBoard.length; i++) {
+                if (winCondition.includes(i)){
+                    gameCells[i].classList.add("winning-game-cell");
+                }
+            }
+            gameOver = true;
+            return;
+        }
+    }
+}
+
+function restartGame() {
+    gameOver = false;
+    gameBoard = [ "", "", "", "", "", "", "", "", "",];
+    for (let cell of gameCells) {
+        cell.innerText = "";
+        cell.classList.remove("winning-game-cell");
+    }
+}
